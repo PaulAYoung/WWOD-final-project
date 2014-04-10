a4f2bd08542eadab7939c71ac061c2fdb99c4eb2
Paul Young <paulandresyoung@gmail.com>
2014-04-10 12:39:28 -0700

    Change data scripts to allow for only getting some data, waiting between downloads, and retrying if download fails.

--- datascrape.py
+++ datascrape.py
@@ -3,17 +3,45 @@
 import os
 import re
 import urllib2
+from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def download_file(url, outpath):
-    """Downloads given url to given path"""
+def confirm(msg):
+    """prints message and asks for y/n response"""
+    while 1:
+        resp = raw_input(msg)
+        if resp in ["y", "Y", "yes", "Yes"]:
+            return True
+        elif resp in ["n", "N", "no", "No"]:
+            return False
+        else:
+            continue
+
+
+def download_file(url, outpath, retry=False):
+    """
+    Downloads given url to given path
+    the retry parameter catches exceptions and asks if the user would like
+    to retry
+    """
     print "downloading {} to {}".format(url, outpath)
 
-    # get file
-    data = urllib2.urlopen(url)
+    #get file, if there is an error, ask if we want to confirm
+    while 1:
+        try:
+            data = urllib2.urlopen(url)
+        except Exception as e:
+            print "Exception caught while downloading {}".format(url)
+            print str(e)
+            if retry and confirm("Retry? (y/n)"):
+                continue
+            else:
+                raise
+        finally:
+            break
 
     # determine extention
     # content disposition has the intended filname
@@ -30,11 +58,18 @@ def download_file(url, outpath):
     outfile.write(data.read())
 
 
-def mass_downloader(url, href_pattern=False, inner_pattern=False):
+def mass_downloader(
+        url,
+        href_pattern=False,
+        inner_pattern=False,
+        wait=0.05,
+        retry=False
+        ):
     """
     Scrapes a page and downloads all files that match given patterns
     href_pattern - pattern to match within link
     inner_pattern - pattern to match in inner html
+    wait - time to wait after a download happens
     e.g:
     <a href="href_pattern">inner_pattern</a>
     """
@@ -63,4 +98,5 @@ def mass_downloader(url, href_pattern=False, inner_pattern=False):
         if get_flag:
             # download file
             outpath = os.path.join("data", inner)
-            download_file(href, link.get(href, outpath))
+            download_file(href, link.get(href, outpath), retry=retry)
+            sleep(wait)
